e1047ebe96e6cbf0eff3d945a2e0a68b52962f0c
Tomasz Soróbka <tomek.sorobka@gmail.com>
2022-12-28 09:22:04 +0100

    added comments for more clarity

--- dashframework-main/app.py
+++ dashframework-main/app.py
@@ -16,33 +16,35 @@ if __name__ == '__main__':
 
     # Create data
     df = px.data.iris()
-    aDb = pd.read_csv('dashframework-main/airbnb_open_data_processed.csv', low_memory=False)
+
+    # Our data bases (I am using "processed" files because I made some small changes to those databases, I can send them to you if you want)
+    airbnbDb = pd.read_csv('dashframework-main/airbnb_open_data_processed.csv', low_memory=False)
     crimeDb = pd.read_csv('dashframework-main/NYPD_Complaint_processed.csv', low_memory=False)
     
-    # Instantiate custom views
-    fig1 = px.scatter(df, x="sepal_width", y="sepal_length")
-    fig2 = px.scatter(aDb, x="review rate number", y="number of reviews", title='Review scatterplot')
-    fig3 = px.scatter_mapbox(aDb, lat="lat", lon="long",
+    # Task visualizations
+    reviewScatterplot = px.scatter(airbnbDb, x="review rate number", y="number of reviews", title='Review scatterplot')
+
+    mapScatter = px.scatter_mapbox(airbnbDb, lat="lat", lon="long",
                         color_discrete_sequence=["fuchsia"], zoom=3, height=900, title='Listings basic map')
-    fig3.update_layout(mapbox_style="open-street-map")
-    fig3.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-    #fig3.show()
+    mapScatter.update_layout(mapbox_style="open-street-map")
+    mapScatter.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 
-    fig4 = px.pie(aDb, values='1', names='host_identity_verified', title='Verified or not')
-    #fig4.show()
+    verifiedPiechart = px.pie(airbnbDb, values='1', names='host_identity_verified', title='Verified or not')
 
+    violinPlot = px.violin(airbnbDb, y="price", x="room type", color="room type", box=True, points="all", title='Violin plot profitalibility analysis')
 
-    # fig5 = px.violin(aDb, y="price", x="room type", color="room type", box=True, points="all", title='Violin plot profitalibility analysis')
-    # fig5.show()
+    crimeHeatmap = px.density_mapbox(crimeDb, lat='Latitude', lon='Longitude', radius=10,
+                        center=dict(lat=0, lon=180), zoom=3,
+                        mapbox_style="stamen-terrain", title='Crime heatmap')
 
-    # fig6 = px.density_mapbox(crimeDb, lat='Latitude', lon='Longitude', radius=10,
-    #                     center=dict(lat=0, lon=180), zoom=3,
-    #                     mapbox_style="stamen-terrain", title='Crime heatmap')
-    # fig6.show()
     
-    reviewScatterplot = dcc.Graph(figure=fig2)
-    basicMap = dcc.Graph(figure=fig3)
-    verifiedPieChart = dcc.Graph(figure=fig4)
+    
+    reviewGraph = dcc.Graph(figure=reviewScatterplot)
+    mapGraph = dcc.Graph(figure=mapScatter)
+    verifiedGraph = dcc.Graph(figure=verifiedPiechart)
+    violinGraph = dcc.Graph(figure=violinPlot)
+    crimeGraph = dcc.Graph(figure=crimeHeatmap)
+
 
     app.layout = html.Div(
             id="app-container",
@@ -59,24 +61,26 @@ if __name__ == '__main__':
                     id="right-column",
                     className="nine columns",
                     children=[
-                        reviewScatterplot,
-                        basicMap,
-                        verifiedPieChart,
-                        #dcc.Graph(figure=fig5),
-                        #dcc.Graph(figure=fig6)
+                        reviewGraph,
+                        mapGraph,
+                        verifiedGraph,
+                        # Here there are not included because they make the whole app go very slow
+                        #violinGraph,
+                        #crimeHeatmap
                     ],
                 ),
             ],
         )
-    # Define interactions
-    @app.callback(
-        Output(reviewScatterplot, "figure"), [
-        Input("select-color-scatter-1", "value"),
-        #Input(basicMap, 'selectedData')
-    ])
-    def update_scatter_1(selected_color):
-        return dcc.Graph(figure=fig2.update_traces(marker=dict(color=selected_color)))
-        #dcc.Graph(figure=px.scatter(aDb, x="review rate number", y="number of reviews", title='Review scatterplot', color=selected_color))
+    # Next task is to define the interactions
+    
+    # @app.callback(
+    #     Output(reviewGraph, "figure"), [
+    #     Input("select-color-scatter-1", "value"),
+    #     Input(mapGraph, 'selectedData')
+    # ])
+    # def update_scatter_1(selected_color):
+    #     return dcc.Graph(figure=reviewScatterplot.update_traces(marker=dict(color=selected_color)))
+        #dcc.Graph(figure=px.scatter(airbnbDb, x="review rate number", y="number of reviews", title='Review scatterplot', color=selected_color))
 
     # @app.callback(
     #     Output(scatterplot1.html_id, "figure"), [
